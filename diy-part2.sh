351f94f59e5621b95848b9098e3050006526c4f4
ZHOUJUNX <40539479+ZHOUJUNX@users.noreply.github.com>
2021-12-25 10:41:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,9 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
+echo '### Updates default IP gate ###'
+
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+echo '### Updates Theme Argon ###'
