fa5d127e2af0b24f63ccf3aa8aab4ced7f3722db
ZHOUJUNX <40539479+ZHOUJUNX@users.noreply.github.com>
2023-07-14 09:03:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,32 +14,28 @@
 sed -i 's/192.168.1.1/172.16.253.254/g' package/base-files/files/bin/config_generate
 echo '### Updates default IP gate ###'
 
-# Updates Theme Argon #
+# argon theme plug-in components #
 rm -rf package/feeds/luci/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/luci-theme-argon
-echo '### Updates Theme Argon ###'
+echo '### luci-theme-argon ###'
 
-# Argon Theme Config plug-in components #
+# argon config plug-in components #
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
-echo '### Argon Theme Config ###'
+echo '### luci-app-argon-config ###'
 
-# luci-app-adguardhome plug-in components #
+# adguardhome plug-in components #
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
 echo '### luci-app-adguardhome ###'
 
-# luci-app-smartdns plug-in components #
+# smartdns plug-in components #
 git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
 echo '### luci-app-smartdns ###'
 
-# OpenAppFilter plug-in components #
-# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-# echo '### OpenAppFilter ###'
-
-# Shutdown plug-in components #
+# shutdown plug-in components #
 git clone https://github.com/ZHOUJUNX/luci-app-poweroff.git package/lean/luci-app-poweroff
 echo '### Shutdown Router ###'
 
-# OpenAppFilter plug-in components #
+# openappfilter plug-in components #
 git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
-echo '### OpenAppFilter ###'
+echo '### luci-app-oaf ###'
 
