b0a125045d960571a6e9054edd4cfa2389a359fe
ZHOUJUNX <40539479+ZHOUJUNX@users.noreply.github.com>
2024-04-15 18:20:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,11 +25,11 @@ echo '### luci-app-argon-config ###'
 
 # adguardhome plug-in components #
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
-echo '### luci-app-adguardhome ###'
+# echo '### luci-app-adguardhome ###'
 
 # smartdns plug-in components #
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
-echo '### luci-app-smartdns ###'
+# echo '### luci-app-smartdns ###'
 
 # shutdown plug-in components #
 git clone https://github.com/ZHOUJUNX/luci-app-poweroff.git package/lean/luci-app-poweroff
@@ -37,8 +37,8 @@ echo '### Shutdown Router ###'
 
 # openappfilter plug-in components #
 # git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
-echo '### luci-app-oaf ###'
+# echo '### luci-app-oaf ###'
 
 # lucky plug-in components #
 # git clone  https://github.com/gdy666/luci-app-lucky.git package/lean
-echo '### luci-app-lucky ###'
+# echo '### luci-app-lucky ###'
